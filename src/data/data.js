576bded169b6e95d25c7fd8c742b2444dcfaa6bc
Chirag_Gautam <chiraggautam8447@gmail.com>
2023-10-09 12:45:09 +0530

    fc

---
+++ src/data/data.js
@@ -0,0 +1,72 @@
+import hotelappimg from '../assets/hotelappimg.jpg';
+import youcloneimg from '../assets/youcloneimg.jpg';
+import Weaimg from '../assets/Weaimg.jpg';
+import chatappimg from '../assets/chatappimg.jpg';
+export const data = [
+  {
+    id: 1,
+    img: hotelappimg,
+    title: 'Hotel Booking Application',
+    desc: 'Developed a fully functional desktop booking application with user authentication and a dynamic user interface using ReactJS and Express JS. Collaborated on database schema and integrated it with the frontend for secure data handling.',
+    link: 'https://mern-chirag-booking-app.onrender.com/',
+    git: 'https://github.com/gautamchirag/Hotel__Booking__Application',
+  },
+  {
+    id: 2,
+    img: youcloneimg,
+    title: 'Modern YouTube Clone',
+    desc: 'Created a fully functional YouTube clone application using React.js and Material UI 5, mimicking the user interface and core functionalities of the popular video-sharing platform.',
+    link: 'https://chirag-yt-clone.onrender.com/',
+    git: 'https://github.com/gautamchirag/Youtube-Clone-App',
+  },
+  {
+    id: 3,
+    img: chatappimg,
+    title: 'Private Chat Application',
+    desc: 'Created a real-time private chat application with secure user authentication using React.js and Firebase. Users can communicate confidentially in real-time.',
+    link: 'https://private-chat-application.onrender.com/',
+    git: 'https://github.com/gautamchirag/PrivateChatApplication',
+  },
+  {
+    id: 4,
+    img: Weaimg,
+    title: 'Weather Application',
+    desc: 'Developed a weather forecasting application that retrieves live data from external APIs, providing users with up-to-date weather information in a user-friendly interface.',
+    link: 'https://gautam-weather-tracking-app.onrender.com/',
+    git: 'https://github.com/gautamchirag/Weather-Tracker-Real-Time-Weather-Forecasting',
+  },
+];
+// const projects = [
+//   {
+//     title: 'Hotel Booking Application',
+//     image: hotelappimg,
+//     description:
+//       'Developed a fully functional desktop booking application with user authentication and a dynamic user interface using ReactJS and Express JS. Collaborated on database schema and integrated it with the frontend for secure data handling.',
+//     demoLink: '#', // Replace with the actual demo link
+//     githubLink: '#', // Replace with the actual GitHub link
+//   },
+//   {
+//     title: 'Modern YouTube Clone',
+//     image: youcloneimg,
+//     description:
+//       'Created a fully functional YouTube clone application using React.js and Material UI 5, mimicking the user interface and core functionalities of the popular video-sharing platform.',
+//     demoLink: '#', // Replace with the actual demo link
+//     githubLink: '#', // Replace with the actual GitHub link
+//   },
+//   {
+//     title: 'Private Chat Application',
+//     image: chatappimg,
+//     description:
+//       'Created a real-time private chat application with secure user authentication using React.js and Firebase. Users can communicate confidentially in real-time.',
+//     demoLink: '#', // Replace with the actual demo link
+//     githubLink: '#', // Replace with the actual GitHub link
+//   },
+//   {
+//     title: 'Weather Application',
+//     image: Weaimg,
+//     description:
+//       'Developed a weather forecasting application that retrieves live data from external APIs, providing users with up-to-date weather information in a user-friendly interface.',
+//     demoLink: '#', // Replace with the actual demo link
+//     githubLink: '#', // Replace with the actual GitHub link
+//   },
+// ];
