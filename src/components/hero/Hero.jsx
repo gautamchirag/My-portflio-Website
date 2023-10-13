e20c504f1d190e99a9013c6d63d87a697e9dbe4f
Chirag_Gautam <109953915+gautamchirag@users.noreply.github.com>
2023-10-13 07:38:45 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -13,14 +13,11 @@ import fi from '../../assets/fi.png';
 
 const Hero = () => {
   return (
-    <div id="home" className={` bg-gradient-to-r from-green-200 to-blue-200 `}>
-      <div className=" container mx-auto pt-5 h-[750px] md:h-[100vh] md:flex-col-reverse sm:h-[780px]  flex sm:flex-col-reverse sm:pt-0 ">
-        <div className=" left mt-4 md:mt-0 flex-1 flex flex-col justify-center gap-5 w-1/2 md:w-full md:py-2 sm:py-0">
+    <div id="home" className="bg-gradient-to-r from-green-200 to-blue-200">
+      <div className="container mx-auto pt-5 h-[750px] md:h-[100vh] md:flex-col-reverse sm:h-[780px] flex sm:flex-col-reverse sm:pt-0">
+        <div className="left mt-4 md:mt-0 flex-1 flex flex-col justify-center gap-5 w-1/2 md:w-full md:py-2 sm:py-0">
           <div className="info w-fit flex flex-col items-start justify-center gap-3 sm:gap-2">
-            <h2
-              data-aos="fade-up"
-              className=" text-5xl font-bold sm:text-[2rem]"
-            >
+            <h2 data-aos="fade-up" className="text-5xl font-bold sm:text-2xl">
               Hello, I'm Chirag Gautam
             </h2>
             <TypeAnimation
@@ -28,7 +25,7 @@ const Hero = () => {
               sequence={[
                 'Frontend Developer',
                 2000,
-                'Mern Stack Developer',
+                'MERN Stack Developer',
                 2000,
                 '',
               ]}
@@ -38,8 +35,7 @@ const Hero = () => {
               className="text-yellow-500 text-4xl font-bold sm:text-3xl"
             />
             <p
-              // data-aos="fade-up"
-              className=" text-[1.1rem] font-medium w-3/4 md:w-full text-gray-600 sm:text-[.95rem]"
+              className="text-[1.1rem] font-medium w-3/4 md:w-full text-gray-600 sm:text-[.95rem]"
             >
               I'm a recent B.Tech graduate from NSUT East Campus
             </p>
@@ -47,13 +43,13 @@ const Hero = () => {
           <div data-aos="fade-up" className="buttons flex gap-5">
             <a
               href="https://www.linkedin.com/in/chirag-gautam-58069a1b8/"
-              className=" bg-black text-[1rem] text-white px-10 py-2 sm:px-8 rounded-lg font-bold  hover:text-yellow-500"
+              className="bg-black text-[1rem] text-white px-10 py-2 sm:px-8 rounded-lg font-bold hover:text-yellow-500"
             >
               <span> Hire Me</span>
             </a>
             <a
               href={chiragResume}
-              className="flex items-center gap-2 border- text-[1rem] bg-white border-black px-7 py-2 sm:px-6 rounded-lg font-bold  hover:text-yellow-500"
+              className="flex items-center gap-2 border text-[1rem] bg-white border-black px-7 py-2 sm:px-6 rounded-lg font-bold hover:text-yellow-500"
               download
             >
               <div className="flex items-center gap-1">
@@ -65,50 +61,49 @@ const Hero = () => {
             <ul
               data-aos="fade-up"
               data-aos-duration="1500"
-              className=" flex gap-5"
+              className="flex gap-5"
             >
               <li>
                 <a href="https://github.com/gautamchirag/">
                   {' '}
-                  <AiFillGithub className=" h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />{' '}
+                  <AiFillGithub className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />{' '}
                 </a>
               </li>
               <li>
                 <a href="https://www.linkedin.com/in/chirag-gautam-58069a1b8/">
                   {' '}
-                  <FaLinkedinIn className=" h-[1.8rem] w-[1.8rem] text-xl hover:scale-125 " />{' '}
+                  <FaLinkedinIn className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />{' '}
                 </a>
               </li>
               <li>
                 <a href="https://instagram.com/chiraggautam991?igshid=OGQ5ZDc2ODk2ZA==">
                   {' '}
-                  <AiFillInstagram className=" h-[1.8rem] w-[1.8rem] text-xl hover:scale-125  " />{' '}
+                  <AiFillInstagram className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />{' '}
                 </a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="right  top-5 flex-1 flex items-center justify-center md:items-end sm:items-end">
+        <div className="right top-5 flex-1 flex items-center justify-center md:items-end sm:items-end">
           <div className="relative h-[88%] w-fit flex items-center sm:items-end">
             <img
               data-aos="fade-up"
-              className=" h-[90%]  w-full object-cover md:h-[95%] md:m-auto sm:m-0"
+              className="h-[90%] w-full object-cover md:h-[95%] md:m-auto sm:m-0"
               src={fi}
-              // src={mine}
               alt="mine"
             />
-            <div className=" absolute bottom-10 md:bottom-3 right-8 md:right-2">
+            <div className="absolute bottom-10 md:bottom-3 right-8 md:right-2">
               <div
                 data-aos="zoom-in"
                 data-aos-duration="1000"
-                className=" relative cursor-pointer"
+                className="relative cursor-pointer"
               >
                 <img
-                  className=" w-[135px] md:w-[90px] circle-text"
+                  className="w-[135px] md:w-[90px] circle-text"
                   src="https://ik.imagekit.io/imgkitt/tr:w-400/Full_Stack_Developer2.png?updatedAt=1683134009107"
                   alt=""
                 />
-                <FaPlay className=" text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" />
+                <FaPlay className="text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" />
               </div>
             </div>
           </div>
